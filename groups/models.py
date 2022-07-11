684927345a21ff05078734c87d6b0c70e635807e
Aayush <just.another.3d.object@gmail.com>
2022-07-12 00:28:58 +0530

    added misaka and imported some functions"

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils.text import slugify
+
+import misaka
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+from django import template
+register = template.Library()
 
 # Create your models here.
