4392c3d84c3a3ebc2d7c5abb23c9ed33f08aa153
Aayush <just.another.3d.object@gmail.com>
2022-07-12 00:50:54 +0530

    Imported functions in posts/models.py

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.urls import reverse
+from django.conf import settings
+
+import misaka
+
+from groups.models import Group
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # Create your models here.
