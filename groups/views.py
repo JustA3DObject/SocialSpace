7b1de18ffee953a4da09dd76c34edf460ff8caca
Aayush <just.another.3d.object@gmail.com>
2022-07-12 22:41:34 +0530

    Created LeaveGroup CBV in groups/views.py

--- groups/views.py
+++ groups/views.py
@@ -33,5 +33,19 @@ class JoinGroup(LoginRequiredMixin, generic.RedirectView):
 
         return super().get(request, *args, **kwargs)
 
-class LeaveGroup():
-    pass
+class LeaveGroup(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse("groups:single", kwargs = {"slug": self.kwargs.get("slug")})
+
+    def get(self, request, *args, **kwargs):
+
+        try:
+            membership = models.GroupMember.objects.filter(user = self.request.user, group__slug = self.kwargs.get("slug")).get()
+        except models.GroupMember.DoesNotExist:
+            messages.warning(self.request, "Hold up! You are not in this group!")
+        else:
+            membership.delete()
+            messages.success(self.request, "Good bye! You have left the group!")
+
+        return super().get(request, *args, **kwargs)
