776c1cbd05e5c62467ae518c2b6f67f57aee6350
Aayush <just.another.3d.object@gmail.com>
2022-07-12 23:51:27 +0530

    Debugging

--- posts/views.py
+++ posts/views.py
@@ -33,9 +33,9 @@ class UserPosts(generic.ListView):
         context["post_user"] = self.post_user
         return context
 
-class PostDetail(SelectRelatedMixin, generic.DetaiView):
+class PostDetail(SelectRelatedMixin, generic.DetailView):
     model = models.Post
-    select_related = ("user", group)
+    select_related = ("user", "group")
 
     def get_queryset(self):
         queryset = super().get_queryset()
