37d9202f311a16597bf3de1d601c58e8bb338f32
Aayush <just.another.3d.object@gmail.com>
2022-07-12 15:39:58 +0530

    Included functions in groups/views.py

--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib.auth.mixin import LoginRequiredMixin, PermissionRequiredMixin
+from django.urls import reverse
+from django.views import generic
 # Create your views here.
